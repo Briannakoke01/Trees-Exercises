f9cfaa897bd34795bd7619b512007d51551c2b20
Briannakoke01 <140966567+Briannakoke01@users.noreply.github.com>
2024-07-25 21:39:09 -0400

    Add files via upload

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    roots: ['<rootDir>/src'],
+    testEnvironment: 'node',
+    moduleFileExtensions: ['js', 'jsx'],
+    transform: {
+      '^.+\\.jsx?$': 'babel-jest',
+    },
+  };
+  
